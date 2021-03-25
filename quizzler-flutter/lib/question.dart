ab0c3acb39039b959c00b14649c1bbe0bc47b94d
Samar <samar1995aziz@gmail.com>
2021-03-25 15:15:56 +0300

    First Commit to Github Repository

---
+++ quizzler-flutter/lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+  // build constructor
+  Question(String q, bool a ){ //no need for {} so we delete it and create new file that hold answer and Q its called quizBrain.dart
+  questionText= q;
+  questionAnswer= a;
+}
+}
+
